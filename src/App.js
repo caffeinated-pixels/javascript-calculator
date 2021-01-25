ecd8f85ee7b4de43b64c8e3a8366649aae57e762
Stevie Gill <29311273+caffeinated-pixels@users.noreply.github.com>
2021-01-25 14:18:38 -0500

    click handlers added

--- src/App.js
+++ src/App.js
@@ -3,12 +3,38 @@ import React, { Component } from 'react'
 export default class App extends Component {
   state = { formulaDisplay: 'formula display', mainDisplay: 0 }
 
+  handleNum = () => {
+    console.log('number clicked')
+  }
+
+  handleOperator = () => {
+    console.log('operator clicked')
+  }
+
+  handleDecimal = () => {
+    console.log('decimal clicked')
+  }
+
+  handleEquals = () => {
+    console.log('equals clicked')
+  }
+
+  handleClear = () => {
+    console.log('AC clicked')
+  }
+
   render() {
     return (
       <main className="calculator-body">
         <FormulaDisplay formulaDisplay={this.state.formulaDisplay} />
         <MainDisplay mainDisplay={this.state.mainDisplay} />
-        <KeyPad />
+        <KeyPad
+          handleNum={this.handleNum}
+          handleOperator={this.handleOperator}
+          handleDecimal={this.handleDecimal}
+          handleEquals={this.handleEquals}
+          handleClear={this.handleClear}
+        />
       </main>
     )
   }
@@ -29,24 +55,60 @@ const MainDisplay = props => {
 const KeyPad = props => {
   return (
     <div className="keypad">
-      <button id="seven">7</button>
-      <button id="eight">8</button>
-      <button id="nine">9</button>
-      <button id="four">4</button>
-      <button id="five">5</button>
-      <button id="six">6</button>
-      <button id="one">1</button>
-      <button id="two">2</button>
-      <button id="three">3</button>
-      <button id="pos-neg">+/-</button>
-      <button id="zero">0</button>
-      <button id="decimal">.</button>
-      <button id="add">+</button>
-      <button id="subtract">-</button>
-      <button id="multiply">X</button>
-      <button id="divide">/</button>
-      <button id="clear">AC</button>
-      <button id="equals">=</button>
+      <button id="seven" onClick={props.handleNum}>
+        7
+      </button>
+      <button id="eight" onClick={props.handleNum}>
+        8
+      </button>
+      <button id="nine" onClick={props.handleNum}>
+        9
+      </button>
+      <button id="four" onClick={props.handleNum}>
+        4
+      </button>
+      <button id="five" onClick={props.handleNum}>
+        5
+      </button>
+      <button id="six" onClick={props.handleNum}>
+        6
+      </button>
+      <button id="one" onClick={props.handleNum}>
+        1
+      </button>
+      <button id="two" onClick={props.handleNum}>
+        2
+      </button>
+      <button id="three" onClick={props.handleNum}>
+        3
+      </button>
+      <button id="pos-neg" onClick={props.handleNum}>
+        +/-
+      </button>
+      <button id="zero" onClick={props.handleNum}>
+        0
+      </button>
+      <button id="decimal" onClick={props.handleDecimal}>
+        .
+      </button>
+      <button id="add" onClick={props.handleOperator}>
+        +
+      </button>
+      <button id="subtract" onClick={props.handleOperator}>
+        -
+      </button>
+      <button id="multiply" onClick={props.handleOperator}>
+        X
+      </button>
+      <button id="divide" onClick={props.handleOperator}>
+        /
+      </button>
+      <button id="clear" onClick={props.handleClear}>
+        AC
+      </button>
+      <button id="equals" onClick={props.handleEquals}>
+        =
+      </button>
     </div>
   )
 }
