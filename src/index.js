64d8af350fa9144131c481928c450bfc0a23b1ed
Stevie Gill <29311273+caffeinated-pixels@users.noreply.github.com>
2022-09-18 07:00:22 -0400

    build: update react, node-sass, scrollbars

--- src/index.js
+++ src/index.js
@@ -1,7 +1,7 @@
-import React from 'react'
-import ReactDOM from 'react-dom'
+import { createRoot } from 'react-dom/client'
 import './main.scss'
 import '@fontsource/lexend-deca'
 import App from './App'
 
-ReactDOM.render(<App />, document.getElementById('root'))
+const root = createRoot(document.getElementById('root'))
+root.render(<App />)
