ed8c78cb7f17a8f13ff5267f2ebf61cbd6b2e834
Stevie Gill <29311273+caffeinated-pixels@users.noreply.github.com>
2022-10-08 10:06:45 -0400

    test: add evaluateFormula tests

--- src/__tests__/App.test.js
+++ src/__tests__/App.test.js
@@ -2,6 +2,9 @@ import { render, screen } from '@testing-library/react'
 import userEvent from '@testing-library/user-event'
 import '@testing-library/jest-dom'
 import App from '../App'
+import { evaluateFormula } from '../helpers/evaluateFormula'
+
+/* need to test evaluateForumla function as well as inputs */
 
 describe('Calculator app', () => {
   it('should render the app', () => {
@@ -9,5 +12,22 @@ describe('Calculator app', () => {
 
     const header = screen.getByText('Mercenary Instruments')
     expect(header).toBeInTheDocument()
+  })
+})
+
+/* big number returns an object, presumably JS automatically 
+serializes to appropriate type? */
+describe('evaluateFormula function', () => {
+  it('should return a string number', () => {
+    const result = evaluateFormula('2+2').toJSON()
+
+    expect(result).toBe('4')
+    expect(typeof result).toBe('string')
+  })
+
+  it('should perform 0.3-0.2 accurately', () => {
+    const result = evaluateFormula('0.3-0.2').toJSON()
+
+    expect(result).toStrictEqual('0.1')
   })
 })
