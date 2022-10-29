3c186d6f526d3be91b5b00dd18b4eaaa3c2e127d
Stevie Gill <29311273+caffeinated-pixels@users.noreply.github.com>
2022-10-29 15:44:21 -0400

    refactor: remove unnecessary conditional

--- src/helpers/processOperatorInput.js
+++ src/helpers/processOperatorInput.js
@@ -1,9 +1,7 @@
 import { appendOperator } from './appendOperator'
 
 export const processOperatorInput = (state, input) => {
-  // need to check if previous calculation has been performed
-
-  return state.isCalcDone || state.formula
+  return state.formula
     ? appendOperator(state, input)
     : { ...state, formula: '0' }
 }
