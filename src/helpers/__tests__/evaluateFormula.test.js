cb90844bd21c4232811401bde18c0ca4c5701c7f
Stevie Gill <29311273+caffeinated-pixels@users.noreply.github.com>
2022-10-29 13:28:43 -0400

    test: add to evaluateFormula

--- src/helpers/__tests__/evaluateFormula.test.js
+++ src/helpers/__tests__/evaluateFormula.test.js
@@ -27,4 +27,16 @@ describe('evaluateFormula function', () => {
     expect(evaluateFormula('6+2*7/2').toJSON()).toStrictEqual('13') // not 28!
     expect(evaluateFormula('5-2+6/3').toJSON()).toStrictEqual('5') // not 3 or 1!
   })
+
+  it('should return the input for incomplete formulas', () => {
+    expect(evaluateFormula('2')).toStrictEqual('2')
+  })
+
+  it('should be able to return infinity', () => {
+    expect(evaluateFormula('50/0').toJSON()).toStrictEqual('Infinity')
+  })
+
+  it('should return inputs that include infinity', () => {
+    expect(evaluateFormula('Infinity+5')).toStrictEqual('Infinity+5')
+  })
 })
