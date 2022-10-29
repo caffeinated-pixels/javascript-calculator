e39921f45ed013e1b8f6d67a2e2e41260db07bd0
Stevie Gill <29311273+caffeinated-pixels@users.noreply.github.com>
2022-10-29 10:49:18 -0400

    chore: update comment

--- src/helpers/addCommasToNum.js
+++ src/helpers/addCommasToNum.js
@@ -3,7 +3,7 @@ export const addCommasToNum = (input) => {
   parts[0] = parts[0].replace(/\B(?=(\d{3})+(?!\d))/g, ',')
   return parts.join('.')
 }
-// TODO: could this be handled with bignumber.js?
+// TODO: could this be handled with bignumber.js toFormat method?
 /**
  * see https://stackoverflow.com/a/2901298/8958062 for explanation of the regex
  * we remove the decimal places before using .replace() and stick back on after
