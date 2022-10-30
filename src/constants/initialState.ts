a7ab219f84313e385d7e8ce05387cb2c98dd7a38
Stevie Gill <29311273+caffeinated-pixels@users.noreply.github.com>
2022-10-30 19:47:16 -0400

    refactor: update maxDigitTimerId type

--- src/constants/initialState.ts
+++ src/constants/initialState.ts
@@ -5,7 +5,7 @@ export type AppState = {
   prevAns: string // store answer for starting new calculation
   isCalcDone: boolean // was last input equals key?
   isMaxDigits: boolean // is currVal at max digits?
-  maxDigitTimerId: number | null // timer id for max digits
+  maxDigitTimerId: NodeJS.Timeout | null // timer id for max digits
 }
 
 export const INITIAL_STATE: AppState = {
