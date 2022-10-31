6eb294b9a0e3e9165f3cccf075cefa07a3ad100a
Stevie Gill <29311273+caffeinated-pixels@users.noreply.github.com>
2022-10-31 08:12:25 -0400

    fix: TS error in evaluateFormula

--- src/helpers/evaluateFormula.ts
+++ src/helpers/evaluateFormula.ts
@@ -1,15 +1,17 @@
 import BigNumber from 'bignumber.js'
 
-const calculate = (a: BigNumber, op: string, b: BigNumber) => {
+type Calculate = (a: BigNumber, op: string, b: BigNumber) => any
+
+const calculate: Calculate = (a, op, b) => {
   switch (op) {
     case '+':
-      return a.plus(b).sd(21)
+      return a.plus(b).sd(21).toString()
     case '-':
-      return a.minus(b).sd(21)
+      return a.minus(b).sd(21).toString()
     case '/':
-      return a.dividedBy(b).sd(21)
+      return a.dividedBy(b).sd(21).toString()
     case '*':
-      return a.multipliedBy(b).sd(21)
+      return a.multipliedBy(b).sd(21).toString()
     default:
       return
   }
@@ -17,7 +19,7 @@ const calculate = (a: BigNumber, op: string, b: BigNumber) => {
 
 export const evaluateFormula = (input: string) => {
   const regArr = ['*/', '+-'] // for building regexes below
-  let output: string // for storing output of iterations
+  let output = '' // for storing output of iterations
 
   for (let i = 0; i < regArr.length; i++) {
     // loop through regexes: 1st iteration = mult/div; 2nd iteration = add/sub
@@ -33,14 +35,10 @@ export const evaluateFormula = (input: string) => {
     while (input.match(re)) {
       // stops when no more matching operations; so we do all the mult/div operations first, then back to the for loop above to start the add/sub operations
 
-      const match = input.match(re) // for access to capture groups
+      const match = input.match(re) as RegExpMatchArray // for access to capture groups
 
       // send matched operations to function below
-      output = calculate(
-        BigNumber(match[1]),
-        match[2],
-        BigNumber(match[3])
-      ).toString()
+      output = calculate(BigNumber(match[1]), match[2], BigNumber(match[3]))
       const outputAsNum = Number(output)
 
       if (isNaN(outputAsNum) || !isFinite(outputAsNum)) return output // exit early if NaN or ∞
