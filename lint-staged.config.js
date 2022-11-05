ff204cebd39a84629f7e7edf0e71e56b97963658
Stevie Gill <29311273+caffeinated-pixels@users.noreply.github.com>
2022-11-05 13:34:06 -0400

    build: switch to js config for lint-staged

---
+++ lint-staged.config.js
@@ -0,0 +1,7 @@
+module.exports = {
+  // run ESLint
+  '**/*.{js,jsx,ts,tsx}': () => 'eslint --max-warnings=0',
+
+  // then run type-check
+  '**/*.{ts,tsx}': () => 'tsc -p tsconfig.json --noEmit',
+}
